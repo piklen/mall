a4dc365bd3f456eb569b2d6f578589227992bfaa
hruoxuan <1524573182@qq.com>
2023-09-02 21:34:12 +0800

    test

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "mall/conf"
 
+func main() {
+	conf.Init()
 }
--- dao/migration.go
+++ dao/migration.go
@@ -1,9 +1,24 @@
 package dao
 
-import "fmt"
+import (
+	"fmt"
+	"mall/model"
+)
 
 func migration() {
-	err := _db.Set("gorm:table_options", "charset=utf8mb4").AutoMigrate()
+	err := _db.Set("gorm:table_options", "charset=utf8mb4").
+		AutoMigrate(
+			&model.User{},
+			&model.Product{},
+			&model.Category{},
+			&model.Address{},
+			&model.Favorite{},
+			&model.Notice{},
+			&model.Order{},
+			&model.ProductImg{},
+			&model.Cart{},
+			&model.Admin{},
+			&model.Carousel{})
 	if err != nil {
 		fmt.Println("err", err)
 	}
