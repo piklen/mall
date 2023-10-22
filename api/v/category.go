03bc0bf1a838ae231652bc7f33d14a27e7e8bdc9
hruoxuan <1524573182@qq.com>
2023-10-22 20:54:35 +0800

    获取商品分类

---
+++ api/v/category.go
@@ -0,0 +1,20 @@
+package v
+
+import (
+	"mall/pkg/util"
+	"mall/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ListCategories(c *gin.Context) {
+	listCategoriesService := service.ListCategoriesService{}
+	if err := c.ShouldBind(&listCategoriesService); err == nil {
+		res := listCategoriesService.List(c.Request.Context())
+		c.JSON(http.StatusOK, res)
+	} else {
+		c.JSON(http.StatusBadRequest, ErrorResponse(err))
+		util.LogrusObj.Infoln(err)
+	}
+}
