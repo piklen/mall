83ba7dd40850096122df211e32b56f3d7210d090
hruoxuan <1524573182@qq.com>
2023-10-22 20:53:33 +0800

    购物车实现

---
+++ api/v/cart.go
@@ -0,0 +1,52 @@
+package v
+
+import (
+	"github.com/gin-gonic/gin"
+	"mall/pkg/util"
+	"mall/service"
+	"net/http"
+)
+
+func CreateCart(c *gin.Context) {
+	createCartService := service.CartService{}
+	claim, _ := util.ParseToken(c.GetHeader("Authorization"))
+	if err := c.ShouldBind(&createCartService); err == nil {
+		res := createCartService.Create(c.Request.Context(), claim.ID)
+		c.JSON(http.StatusOK, res)
+	} else {
+		c.JSON(http.StatusBadRequest, ErrorResponse(err))
+		util.LogrusObj.Infoln(err)
+	}
+}
+
+// 购物车详细信息
+func ShowCarts(c *gin.Context) {
+	showCartsService := service.CartService{}
+	claim, _ := util.ParseToken(c.GetHeader("Authorization"))
+	res := showCartsService.Show(c.Request.Context(), claim.ID)
+	c.JSON(http.StatusOK, res)
+}
+
+// 修改购物车信息
+func UpdateCart(c *gin.Context) {
+	updateCartService := service.CartService{}
+	if err := c.ShouldBind(&updateCartService); err == nil {
+		res := updateCartService.Update(c.Request.Context(), c.Param("id"))
+		c.JSON(http.StatusOK, res)
+	} else {
+		c.JSON(http.StatusBadRequest, ErrorResponse(err))
+		util.LogrusObj.Infoln(err)
+	}
+}
+
+// 删除购物车
+func DeleteCart(c *gin.Context) {
+	deleteCartService := service.CartService{}
+	if err := c.ShouldBind(&deleteCartService); err == nil {
+		res := deleteCartService.Delete(c.Request.Context())
+		c.JSON(http.StatusOK, res)
+	} else {
+		c.JSON(http.StatusBadRequest, ErrorResponse(err))
+		util.LogrusObj.Infoln(err)
+	}
+}
---
+++ service/cart.go
@@ -0,0 +1,119 @@
+package service
+
+import (
+	"context"
+	logging "github.com/sirupsen/logrus"
+	"mall/dao"
+	"mall/model"
+	"mall/pkg/e"
+	"mall/serializer"
+	"strconv"
+)
+
+// CartService 创建购物车
+type CartService struct {
+	Id        uint `form:"id" json:"id"`
+	BossID    uint `form:"boss_id" json:"boss_id"`
+	ProductId uint `form:"product_id" json:"product_id"`
+	Num       uint `form:"num" json:"num"`
+}
+
+func (service *CartService) Create(ctx context.Context, uId uint) serializer.Response {
+	var product *model.Product
+	code := e.Success
+
+	// 判断有无这个商品
+	productDao := dao.NewProductDao(ctx)
+	product, err := productDao.GetProductById(service.ProductId)
+	if err != nil {
+		logging.Info(err)
+		code = e.ErrorDatabase
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+
+	// 创建购物车
+	cartDao := dao.NewCartDao(ctx)
+	cart, status, _ := cartDao.CreateCart(service.ProductId, uId, service.BossID)
+	if status == e.ErrorProductMoreCart {
+		return serializer.Response{
+			Status: status,
+			Msg:    e.GetMsg(status),
+		}
+	}
+
+	userDao := dao.NewUserDao(ctx)
+	boss, _ := userDao.GetUserById(service.BossID)
+	return serializer.Response{
+		Status: status,
+		Msg:    e.GetMsg(status),
+		Data:   serializer.BuildCart(cart, product, boss),
+	}
+}
+
+// Show 购物车
+func (service *CartService) Show(ctx context.Context, uId uint) serializer.Response {
+	code := e.Success
+	cartDao := dao.NewCartDao(ctx)
+	carts, err := cartDao.ListCartByUserId(uId)
+	if err != nil {
+		logging.Info(err)
+		code = e.ErrorDatabase
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+	return serializer.Response{
+		Status: code,
+		Data:   serializer.BuildCarts(carts),
+		Msg:    e.GetMsg(code),
+	}
+}
+
+// Update 修改购物车信息
+func (service *CartService) Update(ctx context.Context, cId string) serializer.Response {
+	code := e.Success
+	cartId, _ := strconv.Atoi(cId)
+
+	cartDao := dao.NewCartDao(ctx)
+	err := cartDao.UpdateCartNumById(uint(cartId), service.Num)
+	if err != nil {
+		logging.Info(err)
+		code = e.ErrorDatabase
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+
+	return serializer.Response{
+		Status: code,
+		Msg:    e.GetMsg(code),
+	}
+}
+
+// 删除购物车
+func (service *CartService) Delete(ctx context.Context) serializer.Response {
+	code := e.Success
+	cartDao := dao.NewCartDao(ctx)
+	err := cartDao.DeleteCartById(service.Id)
+	if err != nil {
+		logging.Info(err)
+		code = e.ErrorDatabase
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+	return serializer.Response{
+		Status: code,
+		Msg:    e.GetMsg(code),
+	}
+}
